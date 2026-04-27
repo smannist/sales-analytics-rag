dec85443d1253fd32528448192caff71cc1551ed
Sami Männistö <sami.mannisto1@gmail.com>
2026-04-28 00:23:04 +0300

    General: lets just use the transformers, instead of being hacky

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,53 +1,11 @@
 from collections.abc import Iterator
 
 import chromadb
-from chromadb.utils.embedding_functions.onnx_mini_lm_l6_v2 import ONNXMiniLM_L6_V2
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
-from langchain_core.embeddings import Embeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 from config import VectorDBConfig
-
-
-# could use sentence-transformers package, but its gigantic
-# default seems fine, so no need to use openai either in this simple app
-class ChromaOnnxEmbeddings(Embeddings):
-    """LC wrapper around Chroma's default all-MiniLM-L6-v2 embeddings.
-
-    Also shrinks tokenizer padding/truncation from Chroma's 256-token default
-    to 112. Our longest doc tokenizes to ~95 tokens, so the default wasted a
-    lot of compute padding short docs out to 256. This made insertion very slow.
-    Bump the value if longer docs are added.
-    """
-
-    def __init__(self) -> None:
-        """The initializer."""
-        self._fn = ONNXMiniLM_L6_V2()
-        self._fn._download_model_if_not_exists()
-        self._fn.tokenizer.enable_padding(pad_id=0, pad_token="[PAD]", length=112)
-        self._fn.tokenizer.enable_truncation(max_length=112)
-
-    def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        """Embed a list of texts into vector representations.
-
-        Args:
-            texts: A list of strings to embed.
-
-        Returns:
-            The embedding vectors.
-        """
-        return [arr.tolist() for arr in self._fn(texts)]
-
-    def embed_query(self, text: str) -> list[float]:
-        """Embed a single query string into a vector representation.
-
-        Args:
-            text: The query string to embed.
-
-        Returns:
-            A single embedding vector.
-        """
-        return self._fn([text])[0].tolist()
 
 
 def populate_vectorstore(
@@ -80,7 +38,9 @@ def get_vectorstore() -> Chroma:
     return Chroma(
         client=chromadb.PersistentClient(path=VectorDBConfig.PATH),
         collection_name=VectorDBConfig.COLLECTION_NAME,
-        embedding_function=ChromaOnnxEmbeddings(),
+        embedding_function=HuggingFaceEmbeddings(
+            model_name="all-MiniLM-L6-v2"
+        ),
     )
 
 
